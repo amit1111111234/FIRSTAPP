cc9cab701cb84fbd656c1133b4c350c1761923c1
amit <amit1schneider@gmail.com>
2025-10-29 16:15:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/FragmentTwo.java
@@ -0,0 +1,16 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.firstapp.R;
+
+public class FragmentTwo extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.firstapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    Button openSecondActivityButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // link UI elements
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        openSecondActivityButton = findViewById(R.id.openSecondActivityButton);
+
+        // handle text-changing button click
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userText = editText.getText().toString();
+                button.setText(userText);
+            }
+        });
+
+        // handle open second activity button click
+        openSecondActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
