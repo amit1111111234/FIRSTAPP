6c5607ed75b059bda7954741f75a83549e12cc33
amit <amit1schneider@gmail.com>
2025-12-02 21:52:00 +0200

    SIGN UP + SIGN IN + FIREWALL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    dependencies(fun DependencyHandlerScope.() {
+        implementation platform ('com.google.firebase:firebase-bom:33.1.2')
+        implementation 'com.google.firebase:firebase-auth'
+    })
 }
